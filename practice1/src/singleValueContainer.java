898c4d95b72daa96437fc68f61e2d0fd7b128102
NAVN <NAVN@NAVNEET-PC>
2019-01-29 22:37:15 +0530

    practice1

---
+++ practice1/src/singleValueContainer.java
@@ -0,0 +1,21 @@
+
+public class singleValueContainer {
+
+	public static void main(String[] args) {
+		byte age=10;
+		System.out.println("age is :"+age);
+		age=12;
+		System.out.println("age now is :"+age);
+		double mat=23.34;
+		System.out.println("mat is :"+mat);
+		char ch='A';
+		ch=66;
+		ch='\u20b9';
+		System.out.println("ch is :"+ch);
+		boolean isInternetConnected=true;
+		isInternetConnected=false;
+		System.out.println("Is Internet connected :"+isInternetConnected);
+
+	}
+
+}
